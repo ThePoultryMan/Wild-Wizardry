33775164be9824faeffff400c80048ca53d4d99b
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-30 21:46:23 -0400

    Initial mod setup

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Mod Template
+# Wild Wizardry
 
 ## About
 
-This is a mod template created by ThePoultryMan on GitHub. It was forked from the [fabric-example-mod](https://github.com/FabricMC/fabric-example-mod).
-The mod has switched to Quilt Mappings and I have changed the format to suit my needs of creating a new mod.
+A Minecraft mod that uses animals to explore a world of magic (and automation).
--- src/main/java/io/github/thepoultryman/wildwizardry/WildWizardry.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/WildWizardry.java
@@ -1,15 +1,15 @@
-package thepoultryman.modid;
+package io.github.thepoultryman.wildwizardry;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModClass implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class WildWizardry implements ModInitializer {
+	public static final String MOD_ID = "wildwizardry";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		
 	}
 }
