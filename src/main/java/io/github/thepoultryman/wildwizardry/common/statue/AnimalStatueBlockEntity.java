90c9901c5254b53ab757c73981c56e255e8a2df9
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-02 22:12:31 -0400

    Move things to parent class

--- src/main/java/io/github/thepoultryman/wildwizardry/common/statue/AnimalStatueBlockEntity.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/common/statue/AnimalStatueBlockEntity.java
@@ -3,10 +3,49 @@ package io.github.thepoultryman.wildwizardry.common.statue;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class AnimalStatueBlockEntity extends BlockEntity {
+public abstract class AnimalStatueBlockEntity extends BlockEntity implements IAnimatable, NamedScreenHandlerFactory {
+    private final AnimationFactory animationFactory = new AnimationFactory(this);
+
     public AnimalStatueBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
+    }
+
+    @Override
+    public Text getDisplayName() {
+        return Text.translatable(this.getCachedState().getBlock().getTranslationKey());
+    }
+
+    @Nullable
+    @Override
+    public ScreenHandler createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+        return null;
+    }
+
+    protected <E extends IAnimatable> PlayState getPlayState(AnimationEvent<E> event) {
+        return PlayState.STOP;
+    }
+
+    @Override
+    public void registerControllers(AnimationData animationData) {
+        animationData.addAnimationController(new AnimationController<>(this, "controller", 0, this::getPlayState));
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.animationFactory;
     }
 }
--- src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/PigStatueBlockEntity.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/PigStatueBlockEntity.java
@@ -1,34 +1,29 @@
 package io.github.thepoultryman.wildwizardry.pig.statue;
 
 import io.github.thepoultryman.wildwizardry.common.statue.AnimalStatueBlockEntity;
+import io.github.thepoultryman.wildwizardry.pig.statue.screen.PigGuiDescription;
 import io.github.thepoultryman.wildwizardry.registry.BlockRegistry;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class PigStatueBlockEntity extends AnimalStatueBlockEntity implements IAnimatable {
-    private final AnimationFactory animationFactory = new AnimationFactory(this);
-
+public class PigStatueBlockEntity extends AnimalStatueBlockEntity {
     public PigStatueBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(BlockRegistry.BlockEntityRegistry.PIG_STATUE_BLOCK_ENTITY, blockPos, blockState);
     }
 
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        return PlayState.STOP; // There are no animations currently
+    @Override
+    public ScreenHandler createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+        return new PigGuiDescription(i, playerInventory);
     }
 
     @Override
-    public void registerControllers(AnimationData animationData) {
-        animationData.addAnimationController(new AnimationController<>(this, "controller", 0, this::predicate));
-    }
-
-    @Override
-    public AnimationFactory getFactory() {
-        return this.animationFactory;
+    protected <E extends IAnimatable> PlayState getPlayState(AnimationEvent<E> event) {
+        return super.getPlayState(event);
     }
 }
