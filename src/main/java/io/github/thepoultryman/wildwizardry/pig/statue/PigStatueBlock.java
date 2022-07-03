814a91ac4afb2063c30eabf073f0b0622b48fa02
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-02 22:13:22 -0400

    Open pig screen on click

--- src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/PigStatueBlock.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/PigStatueBlock.java
@@ -5,17 +5,28 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class PigStatueBlock extends AnimalStatueBlock {
     private static final VoxelShape STATUE_SHAPE;
 
     public PigStatueBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+        return ActionResult.SUCCESS;
     }
 
     @Override
