4439a4a110cff7e12d8e81943de015a78aeaf922
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-02 22:29:34 -0400

    Move things to parent class

--- src/main/java/io/github/thepoultryman/wildwizardry/common/statue/screen/BaseGuiDescription.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/common/statue/screen/BaseGuiDescription.java
@@ -1,12 +1,23 @@
 package io.github.thepoultryman.wildwizardry.common.statue.screen;
 
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription;
+import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.data.Insets;
 import io.github.thepoultryman.wildwizardry.registry.GuiRegistry;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandlerContext;
 
 public class BaseGuiDescription extends SyncedGuiDescription {
+    protected final WGridPanel root;
+
     public BaseGuiDescription(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(GuiRegistry.PIG_STATUE_SCREEN, syncId, playerInventory, getBlockInventory(context, 1), getBlockPropertyDelegate(context));
+
+        this.root = new WGridPanel();
+        this.setRootPanel(this.root);
+        this.root.setSize(300, 200);
+        this.root.setInsets(Insets.ROOT_PANEL);
+
+        this.root.add(this.createPlayerInventoryPanel(), 0, 3);
     }
 }
--- src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/screen/PigGuiDescription.java
+++ src/main/java/io/github/thepoultryman/wildwizardry/pig/statue/screen/PigGuiDescription.java
@@ -1,8 +1,6 @@
 package io.github.thepoultryman.wildwizardry.pig.statue.screen;
 
-import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WItemSlot;
-import io.github.cottonmc.cotton.gui.widget.data.Insets;
 import io.github.thepoultryman.wildwizardry.common.statue.screen.BaseGuiDescription;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandlerContext;
@@ -11,16 +9,9 @@ public class PigGuiDescription extends BaseGuiDescription {
     public PigGuiDescription(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(syncId, playerInventory, context);
 
-        WGridPanel root = new WGridPanel();
-        this.setRootPanel(root);
-        root.setSize(300, 200);
-        root.setInsets(Insets.ROOT_PANEL);
+        WItemSlot slot1 = WItemSlot.of(this.blockInventory, 0);
+        this.root.add(slot1, 4, 1);
 
-        WItemSlot slot1 = WItemSlot.of(playerInventory, 0);
-        root.add(slot1, 4, 1);
-
-        root.add(this.createPlayerInventoryPanel(), 0, 3);
-
-        root.validate(this);
+        this.root.validate(this);
     }
 }
